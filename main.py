e436e5939c3766184527f3dacf205cfd6cf9c4e6
Kunaal Gupta <kunaalgupta@hotmail.com>
2024-07-31 14:56:25 -0600

    File browsing dialog

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
 import MapDesign
+import os
 
+def get_campus_and_building():
+    root = tk.Tk()
+    root.withdraw()
+    building_folder = filedialog.askdirectory(
+        title="Select Building Folder"
+    )
+
+    if not building_folder:
+        print("No folder selected.")
+        return None, None
+    building_name = os.path.basename(building_folder).title()
+    campus_name = os.path.basename(os.path.dirname(building_folder)).title()
+
+    return campus_name, building_name
 
 if __name__ == '__main__':
-    campus = input('Enter Campus Name: ').title()
-    building = input(f'Enter Building Name in {campus}: ').title()
-    # campus = 'North Campus'
-    # building = 'Athabasca Hall'
+    campus, building = get_campus_and_building()
+    print(f'Fetching {building} data from {campus} campus...')
 
-    print()
-    print("Building User interface.....")
-    print('Fetching data....\n')
 
-    app = MapDesign.Application(building, campus)
-    app.mainloop()
+    if campus and building:
+        print("Building User interface.....")
+
+        app = MapDesign.Application(building, campus)
+        app.mainloop()
+    else:
+        print("Application exited without selecting a folder.")
